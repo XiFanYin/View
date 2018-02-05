ee8b05f8242c3b4a2f8cd3e865cbc825aa49b941
XiFanYin <807142217@qq.com>
2018-02-05 09:45:58 +0800

    第一次提交

---
+++ app/src/main/java/viewdemo/tumour/com/a51ehealth/view/MainActivity.java
@@ -0,0 +1,13 @@
+package viewdemo.tumour.com.a51ehealth.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
